1c924cb0e89b8328cf4510d5006585cff56ed3a0
b-junkoo <bjkooo1@gmail.com>
2021-12-21 23:20:17 -0500

    Basic ui: 3 buttons: rock, paper, scissors and displays who won and what the score are

--- app.js
+++ app.js
@@ -1,76 +1,44 @@
-const choices = ["rock", "paper", "scissors"];
-
 function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function playerSelection() {
-    while (true) {
-        let choice = prompt("Let's play rock, paper, scissors.");
-        choice = choice.toLowerCase();
-
-        if (choices.includes(choice)) {
-            return choice;
-        }  else {
-            alert("Invalid choice.")
-        }
-    }
-}
-
-function game() {
+function game(e) {
     let compChoice = computerPlay();
     let compVal = compChoice.length;
-    let playerChoice = playerSelection();
+    let playerChoice = e.target.id;
     let playerVal = playerChoice.length;
 
     if ((playerVal - compVal) == 4) {
-        console.log(`You win, ${playerChoice} beats ${compChoice}`);
-        return "a";
+        results.textContent = `You win, ${playerChoice} beats ${compChoice}`;
+        playerScore+=1;
+        scores.textContent = `Your score: ${playerScore}, computer score: ${compScore}`
+
     } else if ((compVal - playerVal) == 4) {
-        console.log(`You lose ${compChoice} beats ${playerChoice}`);
-        return "b";
+        results.textContent = `You lose ${compChoice} beats ${playerChoice}`;
+        compScore+=1;
+        scores.textContent = `Your score: ${playerScore}, computer score: ${compScore}`
+
     } else if (compVal == playerVal) {
-        console.log("Tie!");
-        return "c";
+        results.textContent = "Tie!";
+
     } else if (compVal > playerVal) {
-        console.log(`You lose ${compChoice} beats ${playerChoice}`)
-        return "d";
+        results.textContent = `You lose ${compChoice} beats ${playerChoice}`;
+        compScore+=1;
+        scores.textContent = `Your score: ${playerScore}, computer score: ${compScore}`
     } else {
-        console.log(`You win, ${playerChoice} beats ${compChoice}`)
-        return "e";
+        results.textContent = `You win, ${playerChoice} beats ${compChoice}`;
+        playerScore+=1;
+        scores.textContent = `Your score: ${playerScore}, computer score: ${compScore}`
     }
 }
 
-function fiveGames() {
-    let playerPoints = 0;
-    let compPoints = 0;
-    let result = game();
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let compScore = 0;
 
-    for ( let games=0; games<5; games++ ) {
-        
-        if (result == "a") {
-            playerChoice++;
-        } else if (result == "b") {
-            compPoints++;
-        } else if (result == "c") {
-            playerPoints++;
-            compPoints++;
-        } else if (result == "d") {
-            compPoints++;
-        } else if (result == "e") {
-            playerPoints++;
-        }
-
-        result = game();
-
-    }
-    if (playerPoints > compPoints) {
-        return `You won ${playerPoints} to ${compPoints}`;
-    } else if (playerPoints < compPoints) {
-        return `You lost ${playerPoints} to ${compPoints}`
-    } else {
-        return `Tie!`
-    }
-}
-
-console.log(fiveGames());
+buttons.forEach((button) => {
+    button.addEventListener('click', e => {
+        game(e);
+    });
+})
